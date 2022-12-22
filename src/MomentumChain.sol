df6e9aff4e18ed390150ec3b989791102780a055
Eddev <alaxsxga.dev@gmail.com>
2022-12-22 14:06:24 +0800

    調整資料儲存的結構

--- src/MomentumChain.sol
+++ src/MomentumChain.sol
@@ -7,38 +7,28 @@ import "openzeppelin-contracts/token/ERC20/IERC20.sol";
 contract MomentumChain is Ownable {
   //大致功能說明：官方部署合約一次，挑戰者可用此合約發起多次不同的挑戰，挑戰相關紀錄都在此合約中
 
-  //該地址的挑戰者資訊
-  mapping (address => ChallengerInfo) public addressToChallengerInfo;
+  //地址的發起過的挑戰id
+  mapping (address => uint[]) public addressToChallengeIds;
   
-  //要存挑戰者數量嗎？想像若要顯示出其他挑戰者內容時，配合上方addressToChallengerInfo使用
-  address[] public challengerAddresses;
-  
-  //挑戰id對應的挑戰者地址，用於判斷是否是挑戰發起者
-  mapping (string => address) public eventIdToChallenger; // eventId => ChallengerＡddress
-  
-  //挑戰id對應的挑戰項目
-  mapping (string => ChallengeEvent) public eventIdToEvent; // eventId => ChallengeEvent
+  //挑戰id對應的挑戰內容
+  mapping (uint => Challenge) public idToChallenge;
 
-  //挑戰者資料與其發起的挑戰內容
-  struct ChallengerInfo {
-    string name; //挑戰者名稱
+  //記錄所有挑戰id，array數量當作id使用
+  uint[] public challengeIds;
 
-    //存挑戰者有多少挑戰項目的id，再去用id去mapping查對應項目細節
-    string[] eventIds;
-    //承上，或是存成巢狀結構？優缺點之類的？目前寫法是用上面的eventIds
-    // ChallengeEvent[] challengeEvents;
-  }
+  enum ChallengeState{ UNINITIATED, PROGRESSING, SUCCEEDED, FAILED, GIVEUP }
 
   //挑戰項目的內容
-  struct ChallengeEvent {
-    string eventId;
+  struct Challenge {
+    address challengeOwner;
     string challengeName;
     string challengeDescription;
     uint challengeDays;
     uint minDays;
     uint betAmount;
     address tokenAddress;
-    bool challengeClosed;
+    ChallengeState state;
+    uint createdAt;
     Record[] records;
   }
 
@@ -53,13 +43,13 @@ contract MomentumChain is Ownable {
   }
   
   //檢查是否是挑戰發起者
-  modifier onlyChallengeOwner(string memory _eventId) {
-    require(msg.sender == eventIdToChallenger[_eventId], "not owner of this challenge");
+  modifier onlyChallengeOwner(uint _challengeId) {
+    require(msg.sender == idToChallenge[_challengeId].challengeOwner, "not owner of this challenge");
     _;
   }
 
   //挑戰者創建挑戰項目
-  function createChallengeEvent(
+  function createChallenge (
       string memory _challengeName,
       string memory _challengeDescription,
       uint _challengeDays,
@@ -67,73 +57,78 @@ contract MomentumChain is Ownable {
       uint _betAmount,
       address _tokenAddress
     ) public {
-    //todo:產生id，確認什麼型態uint, string, byte?
-    string memory _eventId = 'id123';
+      uint _challengeId = challengeIds.length + 1;
+      
+      addressToChallengeIds[msg.sender].push(_challengeId);
+
+      //存挑戰id對應的挑戰內容
+      Record[] memory _records;
+      idToChallenge[_challengeId] = Challenge(
+        msg.sender,
+        _challengeName,
+        _challengeDescription,
+        _challengeDays,
+        _minDays,
+        _betAmount,
+        _tokenAddress,
+        ChallengeState.PROGRESSING,
+        block.timestamp,
+        _records
+        //!!上面的_records會有錯，error msg: Copying of type struct MomentumChain.Record memory[] memory to storage not yet supported.
+        //還沒細查怎麼處理
+      );
+
+      //創建項目時另外打錢進來
+      IERC20(_tokenAddress).transferFrom(msg.sender, address(this), _betAmount);
+  }
+
+  function uploadProgress(uint _challengeId, string memory _note) public onlyChallengeOwner(_challengeId) {
+    Challenge storage challenge = idToChallenge[_challengeId];
+    require(challenge.state == ChallengeState.PROGRESSING, ""); //需要挑戰進行中才可上傳
+    require(challenge.records.length < challenge.challengeDays, ""); //記錄小於總天數才可上傳
+    Record storage lastRecord = challenge.records[challenge.records.length - 1];
+    require(block.timestamp - lastRecord.timestamp >= 10 hours, ""); //超過冷卻時間10小時後才可上傳
+    uint finishAt = challenge.createdAt + challenge.challengeDays * 1 days;
+    require(block.timestamp <= finishAt, ""); //超過總天數後不可上傳
+
+    challenge.records.push(Record(block.timestamp, _note));
     
-    //增加挑戰者名稱與新增挑戰id，若是第一次創建下面寫法應該有問題？
-    ChallengerInfo storage challengerInfo = addressToChallengerInfo[msg.sender];
-    //創建時名字已存在就不修改嗎之類的？
-    challengerInfo.name = _challengeName;
-    challengerInfo.eventIds.push(_eventId);
-    
-    //存挑戰id對應的挑戰者地址，用於判斷是否是發起者，ex:onlyChallengeOwner()
-    eventIdToChallenger[_eventId] = msg.sender;
-
-    //存挑戰id對應的挑戰內容
-    Record[] memory _records;
-    eventIdToEvent[_eventId] = ChallengeEvent(
-      _eventId,
-      _challengeName,
-      _challengeDescription,
-      _challengeDays,
-      _minDays,
-      _betAmount,
-      _tokenAddress,
-      false, //challengeClosed
-      _records 
-      //!!上面的_records會有錯，error msg: Copying of type struct MomentumChain.Record memory[] memory to storage not yet supported.
-      //還沒細查怎麼處理
-    );
-
-    //創建項目時另外打錢進來
-    IERC20(_tokenAddress).transferFrom(msg.sender, address(this), _betAmount);
+    //超過最低天數，標記狀態為已成功
+    if (challenge.records.length >= challenge.minDays) {
+      challenge.state = ChallengeState.SUCCEEDED;
+    }
   }
 
-  function uploadProgress(string memory _eventId, string memory _note) public onlyChallengeOwner(_eventId) {
-    ChallengeEvent storage cEvent = eventIdToEvent[_eventId];
-    require(!cEvent.challengeClosed, ""); //需要挑戰未結束
-    require(cEvent.records.length < cEvent.challengeDays, ""); //記錄小於總天數才可上傳
-    //todo: 冷卻時間限制;
-    cEvent.records.push(Record(block.timestamp, _note));
+  function finishAndWithdrawl(uint _challengeId) public onlyChallengeOwner(_challengeId) {
+    Challenge storage challenge = idToChallenge[_challengeId];
+    require(challenge.state == ChallengeState.SUCCEEDED, ""); //成功狀態會在uploadProgress()中改
+    uint finishAt = challenge.createdAt + challenge.challengeDays * 1 days;
+    require(block.timestamp >= finishAt, ""); //已超過總天數才能領
+    withdrawl(_challengeId, challenge.betAmount);
   }
 
-  function finishAndWithdrawl(string memory _eventId) public onlyChallengeOwner(_eventId) {
-    ChallengeEvent storage cEvent = eventIdToEvent[_eventId];
-    require(!cEvent.challengeClosed, "");
-    require(cEvent.records.length >= cEvent.minDays, ""); //紀錄次數至少高於最低天數，代表挑戰成功
-    //需要檢查做滿天數才能領錢嗎或及格天數就好？若要檢查要怎麼辦到？感覺無法？
-    withdrawl(_eventId, cEvent.betAmount);
-    cEvent.challengeClosed = true;
+  function forceEndChallenge(uint _challengeId) public onlyChallengeOwner(_challengeId) {
+    Challenge storage challenge = idToChallenge[_challengeId];
+    require(challenge.state == ChallengeState.PROGRESSING, "");//TBD:已達最低天數後還可強制終止嗎？
+    uint returnAmount = challenge.betAmount * 9 / 10; //退九成
+    withdrawl(_challengeId, returnAmount);
+    challenge.betAmount -= returnAmount; //修改該挑戰剩餘的金額，到時官方回收
+    challenge.state = ChallengeState.GIVEUP;
   }
 
-  function forceEndChallenge(string memory _eventId) public onlyChallengeOwner(_eventId) {
-    ChallengeEvent storage cEvent = eventIdToEvent[_eventId];
-    require(!cEvent.challengeClosed, "");
-    uint returnAmount = cEvent.betAmount * 9 / 10; //退九成
-    withdrawl(_eventId, returnAmount);
-    cEvent.betAmount -= returnAmount; //修改該挑戰剩餘的金額，到時官方回收
-    cEvent.challengeClosed = true;
+  function confiscate(uint _challengeId) public onlyOwner() {
+    Challenge storage challenge = idToChallenge[_challengeId];
+    require(challenge.state == ChallengeState.FAILED || challenge.state == ChallengeState.GIVEUP, "");  //需要挑戰結束或失敗才能收割
+    //TBD:有幾個情形要討論一下
+    //1.一直是PROGRESSING狀態，例如上傳一次就放著不玩了
+    //2.SUCCEEDED狀態，但一直沒有領
+    //可能寫法：非SUCCEEDED狀態且超過挑戰時限就可回收；SUCCEEDED狀態超過一定天數沒領回收？
+    withdrawl(_challengeId, challenge.betAmount);
   }
 
-  function confiscate(string memory _eventId) public onlyOwner() {
-    ChallengeEvent storage cEvent = eventIdToEvent[_eventId];
-    require(cEvent.challengeClosed, "");  //需要挑戰已結束才能收割
-    withdrawl(_eventId, cEvent.betAmount);
-  }
-
-  function withdrawl(string memory _eventId, uint amount) private {
-    ChallengeEvent storage cEvent = eventIdToEvent[_eventId];
-    IERC20(cEvent.tokenAddress).transferFrom(address(this), msg.sender, amount);
+  function withdrawl(uint _challengeId, uint amount) private {
+    Challenge storage challenge = idToChallenge[_challengeId];
+    IERC20(challenge.tokenAddress).transferFrom(address(this), msg.sender, amount);
   }
 
 }
